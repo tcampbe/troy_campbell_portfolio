a1f1fd2ce244e2fb87b8b819ea830d0bb4dc831d
tcampbe <tcampb@icloud.com>
2021-11-30 11:08:57 -0600

    learn JS

---
+++ src/scripts/learnJS.js
@@ -0,0 +1,15 @@
+
+
+
+
+// use website to visist as many JS courses as can complete
+// https://medium.com/javarevisited/12-free-courses-to-learn-javascript-and-es6-for-beginners-and-experienced-developers-aa35874c9a32
+
+// https://www.udemy.com/course/javascript-essentials/
+
+// https://www.udemy.com/course/code-your-first-game/
+
+
+
+function makeCoffee( sugar, milk)
+
